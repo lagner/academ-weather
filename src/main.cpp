0a2499d7d0de7aea722f085ce203aa9f4d4d664d
bumbaram <spam@lagner.ru>
2016-08-01 01:28:28 +0700

    clean extra vars

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@ int main(int argc, char *argv[])
     QGuiApplication qtApp(argc, argv);
     QQmlApplicationEngine engine;
 
-    QQmlContext* qmlContext = engine.rootContext();
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
     return qtApp.exec();
